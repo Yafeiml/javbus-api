acb3993081e8a3b32d8e430f8d478b44e75ba7ab
亚飞 王 <348188@qq.com>
2025-09-20 15:22:37 +0800

    提交

--- api/utils.ts
+++ api/utils.ts
@@ -1,7 +1,20 @@
-import { JAVBUS } from './constants.js';
+import { JAVBUS, JAVBUS_IMG } from './constants.js';
 
 export const PAGE_REG = /^[1-9]\d*$/;
 
 export function formatImageUrl(url?: string) {
-  return url && !/^http/.test(url) ? `${JAVBUS}${url}` : url;
+    if (!url) return url;
+
+    // 相对路径：拼接 JAVBUS_IMG
+    if (!/^http/.test(url)) {
+        return `${JAVBUS_IMG}${url}`;
+    }
+
+    // 如果是 javbus 其它语言域名，替换成 JAVBUS_IMG
+    if (url.includes('javbus.com/ja')) {
+        return url.replace(/https?:\/\/[^/]+/, JAVBUS_IMG);
+    }
+
+    // 其它情况（外部资源），保持原样
+    return url;
 }
